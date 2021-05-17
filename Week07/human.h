3da0d4318f4168ef4b6979ff47e78c1d11a33261
Qute_Sparrow <dlckdgml345@naver.com>
2021-05-17 23:23:20 +0900

    GD

--- Week07/human.h
+++ Week07/human.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 class human {
@@ -8,6 +10,12 @@ class human {
 public:
 	string sMyName;
 	int nMyMoney;
+	string Name = "이름 : ";
+	string Leftover = "남은";
+	string fruit = "과일";
+	string Leftover_fruit = Leftover + fruit + ": ";
+	string Money = "소지금 : ";
+	string apple_seller = "사과장수";
 	
 public:
 
